7bafbc3a06745106e71eac24ecc15ca05e45e578
Kevin Killingsworth <kevin@redfenix.com>
2017-10-04 06:52:24 -0500

    Store Promotions: Create/Update/Delete handlers
    This adds support for creating/updating/deleting promotions via their
    respective coupon or product API calls.

--- client/extensions/woocommerce/state/sites/promotions/actions.js
+++ client/extensions/woocommerce/state/sites/promotions/actions.js
@@ -2,6 +2,9 @@
  * Internal dependencies
  */
 import {
+	WOOCOMMERCE_PROMOTION_CREATE,
+	WOOCOMMERCE_PROMOTION_DELETE,
+	WOOCOMMERCE_PROMOTION_UPDATE,
 	WOOCOMMERCE_PROMOTIONS_REQUEST,
 } from 'woocommerce/state/action-types';
 
@@ -10,6 +13,36 @@ export function fetchPromotions( siteId, perPage = undefined ) {
 		type: WOOCOMMERCE_PROMOTIONS_REQUEST,
 		siteId,
 		perPage,
+	};
+}
+
+export function createPromotion( siteId, promotion, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_PROMOTION_CREATE,
+		siteId,
+		promotion,
+		successAction,
+		failureAction,
+	};
+}
+
+export function updatePromotion( siteId, promotion, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_PROMOTION_UPDATE,
+		siteId,
+		promotion,
+		successAction,
+		failureAction,
+	};
+}
+
+export function deletePromotion( siteId, promotion, successAction, failureAction ) {
+	return {
+		type: WOOCOMMERCE_PROMOTION_DELETE,
+		siteId,
+		promotion,
+		successAction,
+		failureAction,
 	};
 }
 
