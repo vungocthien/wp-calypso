1316c284f02b4879c23aceaabcb0576d20b723ef
Andrei Demian <undemian@gmail.com>
2017-10-10 10:04:19 +0300

    Update naming

--- client/me/help/help-contact/index.jsx
+++ client/me/help/help-contact/index.jsx
@@ -139,7 +139,7 @@ const HelpContact = React.createClass( {
 		const { howCanWeHelp, howYouFeel, message, site } = contactForm;
 
 		this.props.sendUserInfo( howCanWeHelp, howYouFeel, site );
-		this.props.sendHappychatMessage( message, { isFirst: true } );
+		this.props.sendHappychatMessage( message, { includeInSummary: true } );
 
 		analytics.tracks.recordEvent( 'calypso_help_live_chat_begin', {
 			site_plan_product_id: ( site ? site.plan.product_id : null ),
