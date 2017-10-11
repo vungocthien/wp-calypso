ce5905ba006793a55d8b950f8491020bf1ad4861
Bernhard Reiter <ockham@raz.or.at>
2017-10-11 18:51:29 +0200

    TooDifficult: Cover other plans

--- client/my-sites/site-settings/disconnect-site/too-difficult.jsx
+++ client/my-sites/site-settings/disconnect-site/too-difficult.jsx
@@ -3,37 +3,70 @@
  * External dependencies
  */
 import React from 'react';
+import { connect } from 'react-redux';
 import { localize } from 'i18n-calypso';
-import { map } from 'lodash';
+import { get, map } from 'lodash';
 
 /**
  * Internal dependencies
  */
 import Card from 'components/card';
 import CompactCard from 'components/card/compact';
+import QuerySitePlans from 'components/data/query-site-plans';
+import { getSitePlanSlug } from 'state/sites/selectors';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import {
+	PLAN_JETPACK_PERSONAL,
+	PLAN_JETPACK_PREMIUM,
+	PLAN_JETPACK_BUSINESS,
+} from 'lib/plans/constants';
 
-// TODO: Cover all plans!
-
-function getPersonalPlanFeatures( translate ) {
-	return {
-		backups: translate( 'Backups' ),
-		antispam: translate( 'Antispam' ),
-		stats: translate( 'Stats' ),
-		publicize: translate( 'Publicize' ),
-		subscriptions: translate( 'Subscriptions' ),
-		other: translate( 'Other' ),
-	};
-}
-
-const TooDifficult = ( { translate } ) => (
+const TooDifficult = ( { features, siteId, translate } ) => (
 	<div>
+		<QuerySitePlans siteId={ siteId } />
 		<Card className="disconnect-site__question">
 			{ translate( 'Which feature or service caused you problems?' ) }
 		</Card>
-		{ map( getPersonalPlanFeatures( translate ), ( label, slug ) => (
-			<CompactCard key={ slug }>{ label }</CompactCard>
-		) ) }
+		{ map( features, ( label, slug ) => <CompactCard key={ slug }>{ label }</CompactCard> ) }
 	</div>
 );
 
-export default localize( TooDifficult );
+export default localize(
+	connect( ( state, { translate } ) => {
+		const siteId = getSelectedSiteId( state );
+		const planSlug = getSitePlanSlug( state, siteId );
+		const features = {
+			[ PLAN_JETPACK_PERSONAL ]: {
+				backups: translate( 'Backups' ),
+				antispam: translate( 'Antispam' ),
+				stats: translate( 'Stats' ),
+				publicize: translate( 'Publicize' ),
+				subscriptions: translate( 'Subscriptions' ),
+				other: translate( 'Other' ),
+			},
+			[ PLAN_JETPACK_PREMIUM ]: {
+				backups: translate( 'Backups' ),
+				security: translate( 'Security Scanning' ),
+				antispam: translate( 'Antispam' ),
+				stats: translate( 'Stats' ),
+				publicize: translate( 'Publicize' ),
+				subscriptions: translate( 'Subscriptions' ),
+				other: translate( 'Other' ),
+			},
+			[ PLAN_JETPACK_BUSINESS ]: {
+				backups: translate( 'Backups' ),
+				security: translate( 'Security Scanning' ),
+				antispam: translate( 'Antispam' ),
+				stats: translate( 'Stats' ),
+				publicize: translate( 'Publicize' ),
+				subscriptions: translate( 'Subscriptions' ),
+				other: translate( 'Other' ),
+			},
+		};
+
+		return {
+			features: get( features, planSlug ),
+			siteId,
+		};
+	} )( TooDifficult )
+);
